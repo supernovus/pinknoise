78f96f7da63a103edff60151475b9579e414db95
Timothy Totten <2010@huri.net>
2013-05-28 15:36:38 -0700

    Initial ideas in place.

---
+++ README.md
@@ -0,0 +1,17 @@
+# PinkNoise
+
+## Introduction
+
+The latest of my content management systems.
+Inspired by WhiteNoise, GreyNoise, BlackNoise, and the Webtoo Website engines,
+PinkNoise is the next generation of CMS engines, that builds static websites,
+dynamically.
+
+## Author 
+
+Timothy Totten, supernovus on #perl6, https://github.com/supernovus/
+
+## License
+
+Artistic License 2.0
+
---
+++ lib/PinkNoise.pm
@@ -0,0 +1,29 @@
+package PinkNoise;
+# ABSTRACT: Building static websites, dynamically.
+
+use v5.12;
+use version 0.77;
+our $VERSION = 'v0.1.0';
+
+=head1 SYNOPSIS
+
+PinkNoise is a set of libraries and applications for building web content.
+This package exists solely for meta data purposes.
+
+=head1 SEE ALSO
+
+=over 1
+
+=item *
+
+L<PinkNoise::Render>
+
+=item *
+
+L<PinkNoise::Database>
+
+=back
+
+=cut
+
+1;
